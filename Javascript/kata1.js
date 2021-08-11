006ac8a79681ac0376d11c28aa87a182614710c4
Mauricio Oruezabal <moruezabal@gmail.com>
2021-08-11 17:35:58 -0300

    resolved 'IQ test'

--- Javascript/kata1.js
+++ Javascript/kata1.js
@@ -106,4 +106,35 @@ function arrayDiff(a, b) {
 }
 */
 
+/* Bob is preparing to pass IQ test. The most frequent task in this test is to find out which one 
+of the given numbers differs from the others. Bob observed that one number usually differs from the others 
+in evenness. Help Bob — to check his answers, he needs a program that among the given numbers finds one that
+ is different in evenness, and return a position of this number.
+! Keep in mind that your task is to help Bob solve a real IQ test, 
+which means indexes of the elements start from 1 (not 0) */
 
+function iqTest(numbers){
+  let cantOdd = 0, cantEven = 0, index= 0;
+  numbers = numbers.split(" ").map(Number);
+  while(index < numbers.length && (cantOdd <= 1 && cantEven <= 1 )){
+      numbers[index] %2 == 0 ? cantEven++ : cantOdd++;
+      index++;
+  }
+  cantOdd > 1 ? index = numbers.findIndex(number => number %2 == 0) : index = numbers.findIndex(number => number %2 != 0);
+  return index + 1
+}
+
+/* console.log(iqTest("2 2 1 2 2"));
+console.log(iqTest("5 2 0 4 2"));
+console.log(iqTest("2 3 1 7 5"));
+console.log(iqTest("3 3 1 1 0")); */
+
+/* Most optimal solution:
+function iqTest(numbers){
+  numbers = numbers.split(" ").map(function(el){return parseInt(el)});
+  
+  var odd = numbers.filter(function(el){ return el % 2 === 1});
+  var even = numbers.filter(function(el){ return el % 2 === 0});
+  
+  return odd.length < even.length ? (numbers.indexOf(odd[0]) + 1) : (numbers.indexOf(even[0]) + 1);
+} */
