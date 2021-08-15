25009626b9a2167ad00a0e2783e679ba2b639030
Mauricio Oruezabal <moruezabal@gmail.com>
2021-08-15 18:27:46 -0300

    resolved 'Multiple of 3 or 5'

--- Javascript/kata1.js
+++ Javascript/kata1.js
@@ -161,4 +161,30 @@ function rgb(r, g, b){
   return (args.map(arg => arg.toString(16).padStart(2,0)).join("")).toUpperCase();
 }
 
-console.log(rgb(-20,0,211));
+//console.log(rgb(-20,0,211));
+
+/*Multiple of 3 or 5 */
+/*If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. 
+The sum of these multiples is 23.
+Finish the solution so that it returns the sum of all the multiples of 3 or 5 below the number passed in. 
+Additionally, if the number is negative, return 0 (for languages that do have them).
+
+Note: If the number is a multiple of both 3 and 5, only count it once. */
+
+function solution(number){
+  let sum = 0
+  if (number >= 0){
+    while (number > 0){
+      number--
+      if (number % 3 == 0 ||  number % 5 == 0){
+        sum += number;
+      }
+    }
+    return sum;
+  } 
+  else{
+    return 0;
+  }
+}
+
+console.log(solution(10));
